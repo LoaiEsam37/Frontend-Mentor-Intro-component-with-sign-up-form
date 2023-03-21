2c3170349008daf27961b77c148d050048b575f9
LoaiEsam37 <loaiesam1234@gmail.com>
2023-03-21 11:46:33 +0200

    commit

---
+++ project/src/App.jsx
@@ -0,0 +1,13 @@
+import "./styles/App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <main>
+        <div class="container"></div>
+      </main>
+    </div>
+  );
+}
+
+export default App;
